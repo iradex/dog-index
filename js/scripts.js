7a6308ea5191c4257c8e056e75dffdb7d9cf0968
Matthias <matthiasscharnowski@gmail.com>
2019-08-15 20:58:27 +0200

    Added Bootstrap, changed styling. Navbar links lead to nowhere on purpose.

--- js/scripts.js
+++ js/scripts.js
@@ -1,109 +1,61 @@
-var dogIndex = (function () { var dogRepository = (function () {
+var dogIndex = (function () {
+	var dogRepository = (function () {
 
-   var repository = [];
-    var apiUrl =  'https://dog.ceo/api/breeds/list/all';
-    
-    function loadList() {
-      return $.ajax(apiUrl, {dataType: 'json'}).then(function (json) {
-       repository = Object.keys(json.message);
-       return repository;
-      }).catch(function (e) {
-        console.error(e);
-      })
-    }
+		var repository = [];
+		var apiUrl = 'https://dog.ceo/api/breeds/list/all';
 
-        function getAll() {
-          return repository;
-        }
+		function loadList() {
+			return $.ajax(apiUrl, {
+				dataType: 'json'
+			}).then(function (json) {
+				repository = Object.keys(json.message);
+				return repository;
+			}).catch(function (e) {
+				console.error(e);
+			})
+		}
 
-        function addListItem(dog) {
-            var list = $('.pokemon-list');
-            var listItem = $('<li></li>');
-            var button = $('<button>' + dog + '</button>');
-            button.addClass("btn");
-            listItem.append(button);
-            list.append(listItem);
-           button.on('click', function(){
-                showDetails(dog);
-            });
-        }
+		function getAll() {
+			return repository;
+		}
 
-        function loadDetails(item) {
-          var url = 'https://dog.ceo/api/breed/' + item + '/images/random';
-          return $.ajax(url, {dataType: 'json'}).then(function (details) {
-            imageUrl = details.message;
-          }).catch(function (e) {
-            console.error(e);
-          });
-        }
-    
+		function addListItem(dog) {
+			var list = $('.list-group');
+			var button = $('<button>' + dog + '</button>');
+			button.addClass("btn btn-lg list-group-item list-group-item-action").attr("data-toggle", "modal").attr("data-target", "#dog-modal").attr("type", "button");
+			list.append(button);
+			$(button).on("click", function () {
+				$(".modal-title").empty();
+				$(".modal-body").empty();
+				dogRepository.loadDetails(dog);
+			});
+		}
 
-        return {
-            getAll: getAll,
-            loadList: loadList,
-            loadDetails: loadDetails,
-            addListItem: addListItem
-        };    
-    
-    })();
+		function loadDetails(item) {
+			var url = 'https://dog.ceo/api/breed/' + item + '/images/random';
+			return $.ajax(url, {
+				dataType: 'json'
+			}).then(function (details) {
+				imageUrl = details.message;
+				$(".modal-title").text(item);
+				$(".modal-body").append('<img src="' + imageUrl + '" class ="dog-pic">');
+			}).catch(function (e) {
+				console.error(e);
+			});
+		}
 
-    dogRepository.getAll();
+		return {
+			getAll: getAll,
+			loadList: loadList,
+			loadDetails: loadDetails,
+			addListItem: addListItem
+		};
 
-    dogRepository.loadList().then(function() {
-        dogRepository.getAll().forEach(dogRepository.addListItem);
-      });
-    
-      function showDetails(item) {
-        dogRepository.loadDetails(item).then(function () {
-          showModal(item, imageUrl)  
-        });
-      }
+	})();
 
-      function showModal(title, imageLink) {
-        var $modalContainer = $('#modal-container');
-        
-        $modalContainer.empty();
-        
-        var modal = $('<div></div>').addClass("modal");
-        
-        var $closeButton = $('<button>Close</button>').addClass('modal-close').on('click', removeModal);
-        
-        var $title = $('<h1>' + title + '</h1>');
+	dogRepository.loadList().then(function () {
+		dogRepository.getAll().forEach(dogRepository.addListItem);
+	});
 
-        var $picture = $('<img>').attr('src', imageLink).addClass('dog-pic');
-    
-        modal.append($closeButton);
-        modal.append($title);
-        modal.append($picture);
-        $modalContainer.append(modal);
-        $modalContainer.addClass('is-visible');
-    
-        function removeModal() {
-          $('#modal-container').removeClass('is-visible');
-        }
-      
-        $(window).on('keydown', (e) => {
-          var $modalContainer = $('#modal-container');
-          if (e.key === 'Escape' && $modalContainer.hasClass('is-visible')) {
-            removeModal();  
-          }
-        });
-      
-        $modalContainer.on('click', (e) => {
-          var target = e.target;
-          if (target === $modalContainer) {
-            removeModal();
-          }
-        });
-      } }) ();
-    
-      
-    
-    
-    
-    
-    
-    
-    
-    
-    
+})();
+
